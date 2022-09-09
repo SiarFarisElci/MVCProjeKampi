e820626835bee8cf8893123d93ce55af44bc0716
Şiar Faris Elçi <siarfariselci@outlook.com>
2022-09-09 12:51:30 +0300

    CategoryController Eklendi

---
+++ Project.MVCUI/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Project.BLL.DesingPatterns.GenericRepository.ConcRep;
+using Project.MVCUI.VMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRepository _cRep;
+
+        public CategoryController()
+        {
+            _cRep = new CategoryRepository();
+        }
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            CategoryVM cvm = new CategoryVM
+            {
+                Categories = _cRep.GetActives()
+            };
+            return View(cvm);
+        }
+    }
+}
