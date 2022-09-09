065e8fa924f4bb2df21e5249218ad1208e1500d7
Şiar Faris Elçi <siarfariselci@outlook.com>
2022-09-09 15:25:17 +0300

    Category Crud işlemleri tamamlandı

---
+++ Project.MVCUI/Controllers/AdminCategoryController.cs
@@ -0,0 +1,86 @@
+﻿using FluentValidation.Results;
+using Project.BLL.DesingPatterns.GenericRepository.ConcRep;
+using Project.BLL.ValidationRules;
+using Project.ENTITIES.Models;
+using Project.MVCUI.VMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryRepository _cRep;
+
+        public AdminCategoryController()
+        {
+            _cRep = new CategoryRepository();
+        }
+
+        // GET: Category
+        public ActionResult GetCategoryList()
+        {
+            CategoryVM cvm = new CategoryVM
+            {
+                Categories = _cRep.GetActives()
+            };
+            return View(cvm);
+        }
+
+        public ActionResult AddCategory()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(category);
+            if (results.IsValid)
+            {
+                _cRep.Add(category);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+
+            }
+
+
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+
+            _cRep.Delete(_cRep.Find(id));
+            return RedirectToAction("GetCategoryList");
+        }
+
+        public ActionResult UpdateCategory(int id)
+        {
+            CategoryVM cvm = new CategoryVM
+            {
+                Category = _cRep.Find(id)
+            };
+            return View(cvm);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            _cRep.Update(category);
+            return RedirectToAction("GetCategoryList");
+        }
+
+    }
+}
---
+++ Project.MVCUI/Controllers/StatisticController.cs
@@ -0,0 +1,50 @@
+﻿using Project.BLL.DesingPatterns.GenericRepository.ConcRep;
+using Project.ENTITIES.Enums;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class StatisticController : Controller
+    {
+        CategoryRepository _cRep;
+        HeadingRepository _hRep;
+        WriteRepository _wRep;
+
+        public StatisticController()
+        {
+            _wRep = new WriteRepository();
+            _hRep = new HeadingRepository();
+            _cRep = new CategoryRepository();   
+        }
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            //Kategori Sayisi
+            TempData["TotalCategory"] = _cRep.GetActives().Count();
+
+            //Yazılım Kategorisine gore ait baslılar
+
+            TempData["SoftwareCategory"] = _hRep.GetActives().Count(x=> x.CategoryID ==2);
+
+            //A Harfi olan yazar sayısı
+
+            TempData["WriteName"] = _wRep.GetActives().Count(x=> x.Name.Contains("A") || x.Name.Contains("a"));
+
+            //Enfazla Baslıga sahip kategori
+
+            TempData["Title"] = _cRep.GetActives().Where(x => x.ID == x.Headings.GroupBy(y => y.CategoryID).OrderByDescending(y => y.Count()).Select(y => y.Key).FirstOrDefault()).Select(z => z.CategoryName).FirstOrDefault();
+
+
+            //Aktif ve pasif arasındaki sayısal fark
+
+            int fark = _cRep.GetActives().Count(x => x.Status != DataStatus.Deleted) - _cRep.GetActives().Count(y=> y.Status == DataStatus.Deleted);
+            TempData["fark"] = fark;
+            return View();
+        }
+    }
+}
